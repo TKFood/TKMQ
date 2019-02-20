16c2693593dd57ef0429d73ecab583bc03e68530
at3158 <tk290@tkfood.com.tw>
2019-02-20 16:08:38 +0800

    ConfigurationManager

--- TKMQ/FrmMQMAIL.cs
+++ TKMQ/FrmMQMAIL.cs
@@ -18,6 +18,8 @@ namespace TKMQ
 {
     public partial class FrmMQMAIL : Form
     {
+
+
         public FrmMQMAIL()
         {
             InitializeComponent();
@@ -26,6 +28,10 @@ namespace TKMQ
         #region FUNCTION
         public void SENDMAIL()
         {
+            string MySMTPCONFIG = ConfigurationManager.AppSettings["MySMTP"];
+            string NAME = ConfigurationManager.AppSettings["NAME"];
+            string PW = ConfigurationManager.AppSettings["PW"];
+
             System.Net.Mail.MailMessage MyMail = new System.Net.Mail.MailMessage();
             MyMail.From = new System.Net.Mail.MailAddress("tk290@tkfood.com.tw");
             MyMail.To.Add("tk290@tkfood.com.tw"); //設定收件者Email
@@ -33,8 +39,8 @@ namespace TKMQ
             MyMail.Subject = "Email Test";
             MyMail.Body = "<h1>HIHI</h1>"; //設定信件內容
             MyMail.IsBodyHtml = true; //是否使用html格式
-            System.Net.Mail.SmtpClient MySMTP = new System.Net.Mail.SmtpClient("officemail.cloudmax.com.tw", 25);
-            MySMTP.Credentials = new System.Net.NetworkCredential("sysmailer@tkfood.com.tw", "@@jy0437");
+            System.Net.Mail.SmtpClient MySMTP = new System.Net.Mail.SmtpClient(MySMTPCONFIG, 25);
+            MySMTP.Credentials = new System.Net.NetworkCredential(NAME, PW);
             try
             {
                 MySMTP.Send(MyMail);
