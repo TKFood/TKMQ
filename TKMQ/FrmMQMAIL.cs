daffe420c4303ac220a1f1008cec35bf7a50a20e
at3158 <tk290@tkfood.com.tw>
2025-09-25 09:21:19 +0800

    reports

--- TKMQ/FrmMQMAIL.cs
+++ TKMQ/FrmMQMAIL.cs
@@ -13387,7 +13387,7 @@ namespace TKMQ
             SQL1 = SETSQLNEW();
             SQL_IN = SETSQLNEW_IN();
             SQL_OUT = SETSQLNEW_OUT();
-            report1.Load(@"REPORT\國內、外業務部業績日報表NEWV6.frx");
+            report1.Load(@"REPORT\國內、外業務部業績日報表NEWV6.frx"); 
 
             //20210902密
             Class1 TKID = new Class1();//用new 建立類別實體
